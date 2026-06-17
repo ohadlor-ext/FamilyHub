f5c9228a8d840cdaf09c9b667a5384d059b2adc5
ohadlor-ext <ohad@extra.co.il>
2026-06-17 20:48:32 +0300

    Fix missing ForeignKey on ChildProfile

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, JSON, Enum
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, JSON, Enum, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 import enum
@@ -45,7 +45,7 @@ class ChildProfile(Base):
     __tablename__ = "child_profiles"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     age = Column(Integer)
     grade = Column(String)
     school = Column(String)
